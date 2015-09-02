0a118f445258681ae9203bae368d2d72abfd4823
Stefan <Stefan_Hahn1@gmx.net>
2015-09-02 11:25:40 +1000

    Added ability to display two objects at the same time

--- Assignment1/js/assignment1.js
+++ Assignment1/js/assignment1.js
@@ -2,26 +2,39 @@ var Assignment1 = (function() {
 	var canvas = null;
 	var gl = null;
 	var meshData = null;
-	var projectionMatrix = mat4.create();
-	var rotationAxis = vec3.fromValues(0, 1, 0);
-	var rotationMatrix = mat4.create();
-	var translationMatrix = mat4.create();
-	var startPoint = vec3.fromValues(-2, 0, 0);
-	var endFromStartPoint = vec3.fromValues(2, 0, 0);
-	var endPoint = vec3.create();
 	var vertShader = null;
 	var fragShader = null;
 	var program = null;
+	var colors = [];
+	var projectionMatrix = mat4.create();
+	var rotationAxis = vec3.fromValues(0, 1, 0);
+	var rotationMatrix = mat4.create();
 	
-	var x_speed = 0.01;
-	var x_direction = 1;
-	var current_x = 0;
-	var y_speed = 0;
-	var current_y = 0;
-	var y_direction = 1;
+	var ob1_translationMatrix = mat4.create();
+	var ob1_startPoint = vec3.fromValues(-2, 1, 0);
+	var ob1_endFromStartPoint = vec3.fromValues(2, 0, 0);
+	var ob1_endPoint = vec3.create();
+	var ob1_x_speed = 0.01;
+	var ob1_x_direction = 1;
+	var ob1_current_x = 0;
+	var ob1_y_speed = 0;
+	var ob1_current_y = 0;
+	var ob1_y_direction = 1;
+	
+	var ob2_translationMatrix = mat4.create();
+	var ob2_startPoint = vec3.fromValues(-2, -1, 0);
+	var ob2_endFromStartPoint = vec3.fromValues(2, 0, 0);
+	var ob2_endPoint = vec3.create();
+	var ob2_x_speed = 0.02;
+	var ob2_x_direction = 1;
+	var ob2_current_x = 0;
+	var ob2_y_speed = 0;
+	var ob2_current_y = 0;
+	var ob2_y_direction = 1;
 	
 	var shadersVariables = {
 		vPosition:		null,
+		vColor:			null,
 		projectionMatrix:	null,
 		translationMatrix:	null,
 		rotationMatrix:		null
@@ -38,15 +51,21 @@ var Assignment1 = (function() {
 			
 			vec3.normalize(rotationAxis, rotationAxis);
 			
-			vec3.add(endPoint, startPoint, endFromStartPoint);
+			vec3.add(ob1_endPoint, ob1_startPoint, ob1_endFromStartPoint);
+			vec3.add(ob2_endPoint, ob2_startPoint, ob2_endFromStartPoint);
 			
-			mat4.identity(translationMatrix);
-			mat4.translate(translationMatrix, translationMatrix, startPoint);
+			mat4.identity(ob1_translationMatrix);
+			mat4.translate(ob1_translationMatrix, ob1_translationMatrix, ob1_startPoint);
+			
+			mat4.identity(ob2_translationMatrix);
+			mat4.translate(ob2_translationMatrix, ob2_translationMatrix, ob2_startPoint);
 			
 			initWebGLContext();
 			loadMeshData();
+			// makeColorData();
 			loadShaders();
 			linkProgram();
+			// loadColor();
 			loadData();
 			bindShaders();
 			startWebGL();
@@ -67,6 +86,17 @@ var Assignment1 = (function() {
 		meshData = ObjectLoader.loadObjDataFromHttp('./obj/Monkey.obj');
 	};
 	
+	var makeColorData = function() {
+		var numvert = meshData.vertices.length / 3;
+		for(var i=0;i<numvert;i++) {
+			colors.push(0.0);
+			//colors.push(0.5+(Math.random()*.5));
+			colors.push(1.0);
+			colors.push(1.0);
+			colors.push(1.0);
+		}
+	};
+	
 	var loadShaders = function() {
 		vertShader = ShaderLoader.loadShaderFromHttp(gl, './shaders/vertexShader.glsl', gl.VERTEX_SHADER);
 		fragShader = ShaderLoader.loadShaderFromHttp(gl, './shaders/fragmentShader.glsl', gl.FRAGMENT_SHADER);
@@ -80,10 +110,15 @@ var Assignment1 = (function() {
 	
 	var loadData = function() {
 		var bufferID = gl.createBuffer();
-		
 		gl.bindBuffer(gl.ARRAY_BUFFER, bufferID);
 		gl.bufferData(gl.ARRAY_BUFFER, flatten(meshData.vertices), gl.STATIC_DRAW);
 	};
+	
+	var loadColor = function() {
+		var cBuffer = gl.createBuffer();
+		gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+		gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+	}
 	
 	var bindShaders = function() {
 		shadersVariables.vPosition = gl.getAttribLocation(program, 'vPosition');
@@ -94,10 +129,14 @@ var Assignment1 = (function() {
 		gl.uniformMatrix4fv(shadersVariables.projectionMatrix, false, projectionMatrix);
 		
 		shadersVariables.translationMatrix = gl.getUniformLocation(program, 'translationMatrix');
-		gl.uniformMatrix4fv(shadersVariables.translationMatrix, false, translationMatrix);
+		gl.uniformMatrix4fv(shadersVariables.translationMatrix, false, ob1_translationMatrix);
 		
 		shadersVariables.rotationMatrix = gl.getUniformLocation(program, 'rotationMatrix');
 		gl.uniformMatrix4fv(shadersVariables.rotationMatrix, false, rotationMatrix);
+		
+		shadersVariables.vColor = gl.getAttribLocation(program, 'fColor');
+		gl.vertexAttribPointer( shadersVariables.vColor, 4, gl.FLOAT, false, 0, 0 );
+		gl.enableVertexAttribArray(shadersVariables.vColor);
 	};
 	
 	var startWebGL = function() {
@@ -110,22 +149,35 @@ var Assignment1 = (function() {
 	var currentLocation = vec3.create();
 	var render = function() {
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-		gl.uniformMatrix4fv(shadersVariables.translationMatrix, false, translationMatrix);
-		gl.uniformMatrix4fv(shadersVariables.rotationMatrix, false, rotationMatrix);
-		gl.drawArrays(gl.LINES	, 0, meshData.vertices.length);
 		
+		gl.uniformMatrix4fv(shadersVariables.translationMatrix, false, ob1_translationMatrix);
+		gl.uniformMatrix4fv(shadersVariables.rotationMatrix, false, rotationMatrix);
+		gl.drawArrays(gl.TRIANGLES, 0, meshData.vertices.length);
+		
+		gl.uniformMatrix4fv(shadersVariables.translationMatrix, false, ob2_translationMatrix);
+		gl.uniformMatrix4fv(shadersVariables.rotationMatrix, false, rotationMatrix);
+		gl.drawArrays(gl.TRIANGLES, 0, meshData.vertices.length);
 		
 		/*WebGLHelper.*/requestAnimationFrame(function() {
 			// animate
 			// TODO: animate translation
-			mat4.translate(translationMatrix, translationMatrix, vec3.fromValues(x_direction * x_speed, y_direction * y_speed, 0));
+			mat4.translate(ob1_translationMatrix, ob1_translationMatrix, vec3.fromValues(ob1_x_direction * ob1_x_speed, ob1_y_direction * ob1_y_speed, 0));
+			mat4.translate(ob2_translationMatrix, ob2_translationMatrix, vec3.fromValues(ob2_x_direction * ob2_x_speed, ob2_y_direction * ob2_y_speed, 0));
 			
-			if (((translationMatrix[12] >= endPoint[0]) ) || (translationMatrix[12] <= startPoint[0])) {
-				x_direction *= -1;
+			if (((ob1_translationMatrix[12] >= ob1_endPoint[0]) ) || (ob1_translationMatrix[12] <= ob1_startPoint[0])) {
+				ob1_x_direction *= -1;
 			}
 			
-			if ((translationMatrix[13] >= endPoint[1]) || (translationMatrix[13] <= startPoint[1])) {
-				y_direction *= -1;
+			if ((ob1_translationMatrix[13] >= ob1_endPoint[1]) || (ob1_translationMatrix[13] <= ob1_startPoint[1])) {
+				ob1_y_direction *= -1;
+			}
+			
+			if (((ob2_translationMatrix[12] >= ob2_endPoint[0]) ) || (ob2_translationMatrix[12] <= ob2_startPoint[0])) {
+				ob2_x_direction *= -1;
+			}
+			
+			if ((ob2_translationMatrix[13] >= ob2_endPoint[1]) || (ob2_translationMatrix[13] <= ob2_startPoint[1])) {
+				ob2_y_direction *= -1;
 			}
 			
 			
--- Assignment1/shaders/vertexShader.glsl
+++ Assignment1/shaders/vertexShader.glsl
@@ -1,8 +1,12 @@
 attribute vec4 vPosition;
+attribute vec4 vColor;
 uniform mat4 projectionMatrix;
 uniform mat4 rotationMatrix;
 uniform mat4 translationMatrix;
 
+varying vec4 fColor;
+
 void main() {
 	gl_Position = projectionMatrix * translationMatrix * rotationMatrix * vPosition;
+	fColor = vColor;
 }
