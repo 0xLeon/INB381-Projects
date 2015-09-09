dfaa7de174bc6c92f2b1421fd420fa2584aa03a4
Stefan <Stefan_Hahn1@gmx.net>
2015-09-09 11:22:27 +1000

    Added WebGLGraphicsObject class

---
+++ Common/js/WebGLGraphicsObject.js
@@ -0,0 +1,42 @@
+var WebGLGraphicsObject = (function() {
+	var __class = function(gl, meshData) {
+		this.glContext = gl;
+		this.meshData = meshData;
+		this.buffers = {
+			verticesBuffer:		null,
+			vertexIndicesBuffer:	null,
+			normalsBuffer:		null,
+			normalIndicesBuffer:	null
+		};
+		
+		console.log(this);
+		
+		__createBuffers.apply(this, null);
+		__bindData.apply(this, null);
+	};
+	
+	var __createBuffers = function() {
+		this.buffers.verticesBuffer = this.glContext.createBuffer();
+		this.buffers.vertexIndicesBuffer = this.glContext.createBuffer();
+		this.buffers.normalsBuffer = this.glContext.createBuffer();
+		this.buffers.normalIndicesBuffer = this.glContext.createBuffer();
+	};
+	
+	var __bindData = function() {
+		this.glContext.bindBuffer(this.glContext.ARRAY_BUFFER, this.buffers.verticesBuffer);
+		this.glContext.bufferData(this.glContext.ARRAY_BUFFER, WebGLHelper.flattenf32(this.meshData.vertices), this.glContext.STATIC_DRAW);
+		
+		this.glContext.bindBuffer(this.glContext.ELEMENT_ARRAY_BUFFER, this.buffers.vertexIndicesBuffer);
+		this.glContext.bufferData(this.glContext.ELEMENT_ARRAY_BUFFER, WebGLHelper.flattenui16(this.meshData.vertexIndices), this.glContext.STATIC_DRAW);
+		
+		this.glContext.bindBuffer(this.glContext.ARRAY_BUFFER, this.buffers.normalsBuffer);
+		this.glContext.bufferData(this.glContext.ARRAY_BUFFER, WebGLHelper.flattenf32(this.meshData.normals), this.glContext.STATIC_DRAW);
+		
+		this.glContext.bindBuffer(this.glContext.ELEMENT_ARRAY_BUFFER, this.buffers.normalIndicesBuffer);
+		this.glContext.bufferData(this.glContext.ELEMENT_ARRAY_BUFFER, WebGLHelper.flattenui16(this.meshData.normalIndices), this.glContext.STATIC_DRAW);
+	};
+	
+	__class.prototype.constructor = __class;
+	
+	return __class;
+})();
