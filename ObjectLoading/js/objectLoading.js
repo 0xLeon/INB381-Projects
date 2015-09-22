7f921ffe40d59f5c050c6efa36c334b88458c1eb
Stefan <Stefan_Hahn1@gmx.net>
2015-09-22 14:05:27 +1000

    Added JSDoc

--- ObjectLoading/js/objectLoading.js
+++ ObjectLoading/js/objectLoading.js
@@ -1,18 +1,79 @@
+/**
+ * Object representing the ObjectLoading application
+ * 
+ * @type	{Object}
+ */
 var ObjectLoading = (function() {
+	/**
+	 * Canvas node
+	 * 
+	 * @type	{jQuery}
+	 */
 	var canvas = null;
+	
+	/**
+	 * WebGL rendering context
+	 * 
+	 * @type	{WebGLRenderingContext}
+	 */
 	var gl = null;
+	
+	/**
+	 * Monkey mesh object returned from OBJ Loader
+	 * 
+	 * @type	{Object}
+	 */
 	var meshData = null;
+	
+	/**
+	 * Vertex shadr WebGL object
+	 * 
+	 * @type	{WebGLShader}
+	 */
 	var vertShader = null;
+	
+	/**
+	 * Fragment shader WebGL object
+	 * 
+	 * @type	{WebGLShader}
+	 */
 	var fragShader = null;
+	
+	/**
+	 * Linked WebGL program object
+	 * 
+	 * @type	{WebGLProgram}
+	 */
 	var program = null;
 	
+	/**
+	 * Object containing all WebGL shader variables locations for the used shaders
+	 * 
+	 * @type	{Object}
+	 */
 	var shadersVariables = {
 		vPosition:	null
 	};
 	
+	/**
+	 * Vertex WebGL buffer object
+	 * 
+	 * @type	{WebGLBuffer}
+	 */
 	var vertBuffer = null;
+	
+	/**
+	 * Vertices index WebGL buffer object
+	 * 
+	 * @type	{WebGLBuffer}
+	 */
 	var vertIndexBuffer = null;
 	
+	/**
+	 * Initializing constructor for this application
+	 * 
+	 * @param	{jQuery}	_canvas		Canvas node
+	 */
 	var init = function(_canvas) {
 		canvas = $(_canvas);
 		
@@ -33,26 +94,41 @@ var ObjectLoading = (function() {
 		}
 	};
 	
+	/**
+	 * Setting up the WebGL context and viewport
+	 */
 	var initWebGLContext = function() {
 		gl.viewport(0, 0, canvas.get(0).width, canvas.get(0).height);
 		gl.clearColor(1.0, 1.0, 1.0, 1.0);
 	};
 	
+	/**
+	 * Loads the mesh data
+	 */
 	var loadMeshData = function() {
 		meshData = ObjectLoader.loadObjDataFromHttp('./obj/Monkey.obj');
 	};
 	
+	/**
+	 * Loads and compiles the shaders
+	 */
 	var loadShaders = function() {
 		vertShader = ShaderLoader.loadShaderFromHtml(gl, '#vertex-shader', gl.VERTEX_SHADER);
 		fragShader = ShaderLoader.loadShaderFromHtml(gl, '#fragment-shader', gl.FRAGMENT_SHADER);
 	};
 	
+	/**
+	 * Links the shader program
+	 */
 	var linkProgram = function() {
 		program = ShaderLoader.linkProgram(gl, vertShader, fragShader);
 		
 		gl.useProgram(program);
 	};
 	
+	/**
+	 * Creates the buffers and puts the data to the shaders
+	 */
 	var loadData = function() {
 		vertBuffer = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vertBuffer);
@@ -63,6 +139,9 @@ var ObjectLoading = (function() {
 		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, WebGLHelper.flattenui16(meshData.vertexIndices), gl.STATIC_DRAW);
 	};
 	
+	/**
+	 * Binds needed shaders before rendering
+	 */
 	var bindShaders = function() {
 		shadersVariables.vPosition = gl.getAttribLocation(program, 'vPosition');
 		
@@ -71,10 +150,16 @@ var ObjectLoading = (function() {
 		gl.enableVertexAttribArray(shadersVariables.vPosition);
 	};
 	
+	/**
+	 * Initializes the rendering
+	 */
 	var startWebGL = function() {
 		render();
 	};
 	
+	/**
+	 * Renders a frame
+	 */
 	var render = function() {
 		gl.clear(gl.COLOR_BUFFER_BIT);
 		gl.drawElements(gl.LINES, meshData.vertexIndices.length * 3, gl.UNSIGNED_SHORT, 0);
--- Triangle/js/triangle.js
+++ Triangle/js/triangle.js
@@ -1,15 +1,60 @@
+/**
+ * Object representing the Triangle application
+ * 
+ * @type	{Object}
+ */
 var Triangle = (function() {
+	/**
+	 * Canvas node
+	 *
+	 * @type	{jQuery}
+	 */
 	var canvas = null;
+	
+	/**
+	 * WebGL rendering context
+	 *
+	 * @type	{WebGLRenderingContext}
+	 */
 	var gl = null;
+	
+	/**
+	 * Monkey mesh object returned from OBJ Loader
+	 *
+	 * @type	{Object}
+	 */
 	var meshData = null;
+	
+	/**
+	 * Vertex shadr WebGL object
+	 *
+	 * @type	{WebGLShader}
+	 */
 	var vertShader = null;
+	
+	/**
+	 * Fragment shader WebGL object
+	 *
+	 * @type	{WebGLShader}
+	 */
 	var fragShader = null;
+	
+	/**
+	 * Linked WebGL program object
+	 *
+	 * @type	{WebGLProgram}
+	 */
 	var program = null;
 	
 	var shadersVariables = {
 		vPosition: null
 	};
 	
+	/**
+	 * Initializing constructor for this application
+	 *
+	 * @param	{jQuery}	_canvas		Canvas node
+	 */
 	var init = function(_canvas) {
 		canvas = $(_canvas);
 		
@@ -30,11 +75,17 @@ var Triangle = (function() {
 		}
 	};
 	
+	/**
+	 * Setting up the WebGL context and viewport
+	 */
 	var initWebGLContext = function() {
 		gl.viewport(0, 0, canvas.get(0).width, canvas.get(0).height);
 		gl.clearColor(1.0, 1.0, 1.0, 1.0);
 	};
 	
+	/**
+	 * Loads the mesh data
+	 */
 	var loadMeshData = function() {
 		meshData = [
 			vec2.fromValues(-0.5, -0.5),
@@ -44,17 +95,26 @@ var Triangle = (function() {
 		];
 	};
 	
+	/**
+	 * Loads and compiles the shaders
+	 */
 	var loadShaders = function() {
 		vertShader = ShaderLoader.loadShaderFromHtml(gl, '#vertex-shader', gl.VERTEX_SHADER);
 		fragShader = ShaderLoader.loadShaderFromHtml(gl, '#fragment-shader', gl.FRAGMENT_SHADER);
 	};
 	
+	/**
+	 * Links the shader program
+	 */
 	var linkProgram = function() {
 		program = ShaderLoader.linkProgram(gl, vertShader, fragShader);
 		
 		gl.useProgram(program);
 	};
 	
+	/**
+	 * Creates the buffers and puts the data to the shaders
+	 */
 	var loadData = function() {
 		var bufferID = gl.createBuffer();
 		
@@ -62,6 +122,9 @@ var Triangle = (function() {
 		gl.bufferData(gl.ARRAY_BUFFER, WebGLHelper.flattenf32(meshData), gl.STATIC_DRAW);
 	};
 	
+	/**
+	 * Binds needed shaders before rendering
+	 */
 	var bindShaders = function() {
 		shadersVariables.vPosition = gl.getAttribLocation(program, 'vPosition');
 		
@@ -69,10 +132,16 @@ var Triangle = (function() {
 		gl.enableVertexAttribArray(shadersVariables.vPosition);
 	};
 	
+	/**
+	 * Initializes the rendering
+	 */
 	var startWebGL = function() {
 		render();
 	};
 	
+	/**
+	 * Renders a frame
+	 */
 	var render = function() {
 		gl.clear(gl.COLOR_BUFFER_BIT);
 		gl.drawArrays(gl.TRIANGLE_STRIP, 0, meshData.length);
