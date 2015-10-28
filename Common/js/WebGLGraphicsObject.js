da0ae1a570bbc1982894c2216f75381893057770
Stefan <Stefan_Hahn1@gmx.net>
2015-10-29 06:19:17 +1000

    Use correct source for WebGL class constants

--- Common/js/WebGLGraphicsObject.js
+++ Common/js/WebGLGraphicsObject.js
@@ -39,14 +39,14 @@ var WebGLGraphicsObject = (function() {
 	 * @private
 	 */
 	var __bindData = function() {
-		this.glContext.bindBuffer(this.glContext.ARRAY_BUFFER, this.buffers.normalsBuffer);
-		this.glContext.bufferData(this.glContext.ARRAY_BUFFER, WebGLHelper.flattenf32(this.meshData.normalsPerVertex), this.glContext.STATIC_DRAW);
+		this.glContext.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, this.buffers.normalsBuffer);
+		this.glContext.bufferData(WebGLRenderingContext.ARRAY_BUFFER, WebGLHelper.flattenf32(this.meshData.normalsPerVertex), WebGLRenderingContext.STATIC_DRAW);
 		
-		this.glContext.bindBuffer(this.glContext.ARRAY_BUFFER, this.buffers.verticesBuffer);
-		this.glContext.bufferData(this.glContext.ARRAY_BUFFER, WebGLHelper.flattenf32(this.meshData.vertices), this.glContext.STATIC_DRAW);
+		this.glContext.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, this.buffers.verticesBuffer);
+		this.glContext.bufferData(WebGLRenderingContext.ARRAY_BUFFER, WebGLHelper.flattenf32(this.meshData.vertices), WebGLRenderingContext.STATIC_DRAW);
 		
-		this.glContext.bindBuffer(this.glContext.ELEMENT_ARRAY_BUFFER, this.buffers.vertexIndicesBuffer);
-		this.glContext.bufferData(this.glContext.ELEMENT_ARRAY_BUFFER, WebGLHelper.flattenui16(this.meshData.vertexIndices), this.glContext.STATIC_DRAW);
+		this.glContext.bindBuffer(WebGLRenderingContext.ELEMENT_ARRAY_BUFFER, this.buffers.vertexIndicesBuffer);
+		this.glContext.bufferData(WebGLRenderingContext.ELEMENT_ARRAY_BUFFER, WebGLHelper.flattenui16(this.meshData.vertexIndices), WebGLRenderingContext.STATIC_DRAW);
 		
 	};
 	
